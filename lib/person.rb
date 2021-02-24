6b00f37ef203ce4ebf3dc3d0f45170cc9e6a21a4
sir-udenna <udennaboy@gmail.com>
2021-02-24 14:42:35 -0600

    shane changes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
-# your code goes here
+# your code goes her
+require 'pry'
+class Person
+    def initialize(name)
+        @name = name
+        @balance = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def name
+        @name
+    end
+
+    def bank_account
+        @balance
+    end
+
+    def bank_account=(balance)
+        @balance = balance
+    end
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+    end
+
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+    end
+end
